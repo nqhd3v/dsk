ff41a97f784d30282903fa3c72ada5427846cb93
huy-nguyenq-tpv-clv <huy.nguyenq.tpv@one-line.com>
2026-05-29 12:50:26 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       name: "dg-web",
       cwd: "./apps/web",
       script: "node",
-      args: ".next/standalone/apps/web",
+      args: ".next/standalone/apps/web/server.js",
       interpreter: "none",
       instances: 1,
       autorestart: true,
